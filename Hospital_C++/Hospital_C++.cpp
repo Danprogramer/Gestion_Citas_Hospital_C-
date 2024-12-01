96f096cba48c99cf18db4a37e855d4d18bb9b9bc
Daniel Ferré <danielferre1999@icloud.com>
2024-12-01 21:02:57 +0100

    1st Commit: Creación de la primera clase Paciente, falta definir la función eliminarCita

--- Hospital_C++/Hospital_C++.cpp
+++ Hospital_C++/Hospital_C++.cpp
@@ -1,10 +1,39 @@
 ﻿#include "Hospital_C++.h"
 #include "iostream"
+#include "vector"
 
 using namespace std;
 
+class Cita;
+
+class Paciente {
+private:
+    int id;
+    string nombre;
+    string fechaIngreso;
+    string historialClinico;
+    vector<shared_ptr<Cita>> citas;
+
+public:
+    Paciente(int id, const string& nombre, const string& fechaIngreso)
+        : id(id), nombre(nombre), fechaIngreso(fechaIngreso) {
+    }
+
+    void registrarHistorial(const string& historial) {
+        historialClinico += historial + "\n";
+    }
+
+    void agregarCita(shared_ptr<Cita> cita) {
+        citas.push_back(cita);
+    }
+
+    void eliminarCita(int idCita);
+
+    int getId() const { return id; }
+    string getNombre() const { return nombre; }
+};
+
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	
 }
