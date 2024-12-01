52bc5fb68a04ca499327a45ed3d2c19b6fdce52b
Daniel Ferré <danielferre1999@icloud.com>
2024-12-01 21:24:48 +0100

    2nd Commit: Ccreación de la clase paciente, funcion eliminar cita definida, falta corregir el puntero

--- Hospital_C++/Hospital_C++.cpp
+++ Hospital_C++/Hospital_C++.cpp
@@ -2,6 +2,7 @@
 #include "iostream"
 #include "vector"
 
+
 using namespace std;
 
 class Cita;
@@ -12,7 +13,7 @@ private:
     string nombre;
     string fechaIngreso;
     string historialClinico;
-    vector<shared_ptr<Cita>> citas;
+    vector<weak_ptr<Cita>> citas;
 
 public:
     Paciente(int id, const string& nombre, const string& fechaIngreso)
@@ -27,7 +28,41 @@ public:
         citas.push_back(cita);
     }
 
-    void eliminarCita(int idCita);
+    void eliminarCita(int idCita) {
+        auto it = remove_if(citas.begin(), citas.end(), [idCita](weak_ptr<Cita> citaPtr) {
+            if (auto cita = citaPtr.lock()) {
+                return cita->getId() == idCita;
+            }
+            return false;
+            });
+        if (it != citas.end()) {
+            citas.erase(it, citas.end());
+            cout << "Cita con ID " << idCita << " eliminada exitosamente." << endl;
+        }
+        else {
+            cout << "Cita con ID " << idCita << " no encontrada." << endl;
+        }
+    }
+
+    int getId() const { return id; }
+    string getNombre() const { return nombre; }
+};
+
+
+class Medico {
+private:
+    int id;
+    string nombre;
+    string especialidad;
+    bool disponibilidad;
+
+public:
+    Medico(int id, const string& nombre, const string& especialidad)
+        : id(id), nombre(nombre), especialidad(especialidad), disponibilidad(true) {
+    }
+
+    void setDisponibilidad(bool disponible) { disponibilidad = disponible; }
+    bool estaDisponible() const { return disponibilidad; }
 
     int getId() const { return id; }
     string getNombre() const { return nombre; }
